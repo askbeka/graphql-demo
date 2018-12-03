0553ed00d1cfbade08a532b93e7e59ffc401b2ec
askbeka <beknaraskarov@gmail.com>
2018-12-03 22:11:11 +0100

    asd

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { GraphQLServer, PubSub } = require('graphql-yoga')
+const resolvers = require('./resolvers')
+
+
+pubsub = new PubSub();
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: {
+    pubsub,
+  },
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
